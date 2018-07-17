f945c8df712015b7ef5456697a81369d45e9da0a
Jyrone Parker <jyrone.parker@gmail.com>
2018-07-16 23:06:39 -0400

    TODO:implement other functions

--- numbers.py
+++ numbers.py
@@ -1,8 +1,19 @@
-def add(num1,num2):
-    ans = num1 + num2
-    print("The answer is " + str(ans))
-    return num1 + num2
 
-add(1,3)
-add(2,6)
-add(100,123465)
+def add():
+    print('Add two numbers')
+    num1 = int(input('Enter 1st number: ')) #input takes keyboard input
+    num2 = int(input('Enter 2nd number: '))
+    ans = num1+num2
+    print("The answer is " + str(ans))
+    return ans
+
+def sub():
+    return
+def divide():
+    return
+def multiply():
+    return
+def mod():
+    return
+
+add()
